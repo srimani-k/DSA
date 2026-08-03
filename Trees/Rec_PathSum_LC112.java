3f0472ab22b08a251a7fb1d0df618b36e28ed579
srimani-k <kakumanusrimani@gmail.com>
2026-08-03 22:28:54 +0530

    fix

--- Trees/Rec_PathSum_LC112.java
+++ Trees/Rec_PathSum_LC112.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Rec_PathSum_LC112 {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        Scanner scanner = new Scanner(System.in);
+        tree.insert(scanner);
+        tree.display();
+        int targetSum = 22; // Example target sum
+        boolean hasPath = hasPathSum(tree.root, targetSum);
+        System.out.println("Does the binary tree have a root-to-leaf path with sum " + targetSum + "? " + hasPath);
+    }
     private static boolean hasPathSum(BinaryTree.Node root, int targetSum){
         if(root==null){
             return false;
